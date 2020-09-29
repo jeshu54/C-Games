d771ce009f29c4596beda94920dd4526a2103c9f
Avro Mukherjee <60354888+jeshu54@users.noreply.github.com>
2020-09-28 23:01:08 -0400

    Simple Snake Game
    It's a classic snake game. Eat the fruit and grow. Don't bite yourself or hit the wall.

---
+++ SnakeGame.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib>
+
+void setup() {
+
+
+}
+
+void drawmap() {
+
+
+}
+
+void input() {
+
+
+}
+
+void  logic() {
+
+
+}
+
+int main() {
+ // Functions for the game
+
+}
